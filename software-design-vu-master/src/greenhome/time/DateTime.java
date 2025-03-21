432ce72aabde794ab36c27e45814dc74f8c01856
Damian Strzemieczny <damian.strzemieczny@gmail.com>
2025-03-21 14:14:57 +0100

    DateTime.java added

---
+++ software-design-vu-master/src/greenhome/time/DateTime.java
@@ -0,0 +1,12 @@
+package greenhome.time;
+public class DateTime {
+    int day, month, year, hour, minute;
+
+    public DateTime(int day, int month, int year, int hour, int minute) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+    }
+}
